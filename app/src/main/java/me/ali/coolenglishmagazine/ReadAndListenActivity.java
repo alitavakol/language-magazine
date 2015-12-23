c83835f144678df7fdb1dbeb6b56922e432c5408
Ali Tavakol <ali.tavakol@gmail.com>
2015-12-23 20:10:37 +0330

    add emty activity with layout

---
+++ app/src/main/java/me/ali/coolenglishmagazine/ReadAndListenActivity.java
@@ -0,0 +1,13 @@
+package me.ali.coolenglishmagazine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReadAndListenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_and_listen);
+    }
+}
