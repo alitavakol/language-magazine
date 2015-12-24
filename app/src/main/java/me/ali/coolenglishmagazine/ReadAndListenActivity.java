2a8bec6c00b58e96f4d40cb212f08636512454bc
Ali Tavakol <ali.tavakol@gmail.com>
2015-12-24 08:43:52 +0330

    add webview and simple media playback control buttons

--- app/src/main/java/me/ali/coolenglishmagazine/ReadAndListenActivity.java
+++ app/src/main/java/me/ali/coolenglishmagazine/ReadAndListenActivity.java
@@ -1,7 +1,12 @@
 package me.ali.coolenglishmagazine;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
 
 public class ReadAndListenActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class ReadAndListenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read_and_listen);
+
+        // TODO read http://javarticles.com/2015/09/android-toolbar-example.html to add toolbar
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///mnt/sdcard/cool-english-magazine/swimming-squirrel.html");
+
+        Toast.makeText(this, Environment.getExternalStorageDirectory().toString(), Toast.LENGTH_LONG).show();
     }
 }
