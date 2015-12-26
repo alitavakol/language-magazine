da339a27f353a3e35c08fb3699312e0b438a053f
Ali Tavakol <ali.tavakol@gmail.com>
2015-12-26 14:11:41 +0330

    building media playback service...
    custom layout for foreground notification,
    state change listener

--- app/src/main/java/me/ali/coolenglishmagazine/MusicService.java
+++ app/src/main/java/me/ali/coolenglishmagazine/MusicService.java
@@ -1,30 +1,45 @@
 package me.ali.coolenglishmagazine;
 
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.session.PlaybackState;
 import android.os.Binder;
 import android.os.IBinder;
 import android.os.PowerManager;
+import android.support.annotation.NonNull;
+import android.support.v7.app.NotificationCompat;
+import android.widget.RemoteViews;
 
 import me.ali.coolenglishmagazine.util.LogHelper;
 
+/**
+ * Web references:
+ * foreground service: http://www.truiton.com/2014/10/android-foreground-service-example/
+ */
 public class MusicService extends Service implements
         MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener,
         MediaPlayer.OnCompletionListener {
 
-    private static final String TAG = LogHelper.makeLogTag(MusicService.class);
     public static final int PLAYBACK_NOTIFICATION_ID = 100;
 
     public static final String ACTION_PLAY = "me.ali.coolenglishmagazine.ACTION_PLAY";
+    public static final String ACTION_PAUSE = "me.ali.coolenglishmagazine.ACTION_PAUSE";
+    public static final String ACTION_STOP = "me.ali.coolenglishmagazine.ACTION_STOP";
+
+    private static final String TAG = LogHelper.makeLogTag(MusicService.class);
 
     /**
-     * local broadcast intent action sent when media status changes
+     * This is the object that receives interactions from clients.
      */
-    public static final String MUSIC_STATUS_CHANGED = "me.ali.coolenglishmagazine.music_status_changed";
+    private final IBinder binder = new MusicBinder();
 
     private MediaPlayer mediaPlayer = null;
 
@@ -52,34 +67,133 @@ public class MusicService extends Service implements
     public void onPrepared(MediaPlayer player) {
         LogHelper.i(TAG, "Media prepared.");
 
-        PendingIntent pi = PendingIntent.getActivity(getApplicationContext(), 0,
-                new Intent(getApplicationContext(), ReadAndListenActivity.class),
-                PendingIntent.FLAG_UPDATE_CURRENT);
-
-        Notification.Builder builder = new Notification.Builder(getApplicationContext());
-        builder.setAutoCancel(false);
-        builder.setTicker("this is ticker text");
-        builder.setContentTitle("WhatsApp Notification");
-        builder.setContentText("You have a new message");
-        builder.setSmallIcon(R.drawable.ic_launcher);
-        builder.setContentIntent(pi);
-        builder.setOngoing(true);
-        builder.setSubText("This is subtext...");
-
-        startForeground(PLAYBACK_NOTIFICATION_ID, builder.build());
+        startForeground(PLAYBACK_NOTIFICATION_ID, getNotification(true));
+        player.start();
+        if(onMediaStateChangedListener != null)
+            onMediaStateChangedListener.onMediaStateChanged(PlaybackState.STATE_PLAYING);
     }
+
+    // TODO implement audio becoming noisy handler
 
     @Override
     public IBinder onBind(Intent intent) {
         return binder;
     }
 
-    // TODO implement audio becoming noisy handler
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        if(onMediaStateChangedListener != null)
+            onMediaStateChangedListener.onMediaStateChanged(PlaybackState.STATE_STOPPED);
+    }
 
-    /**
-     * This is the object that receives interactions from clients.
-     */
-    private final IBinder binder = new MusicBinder();
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        return false;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent.getAction().equals(ACTION_PLAY)) {
+            LogHelper.i(TAG, "Received start foreground intent ");
+
+            handlePlayRequest(intent.getStringExtra("dataSource"));
+
+        } else if (intent.getAction().equals("PREV_ACTION")) {
+            LogHelper.i(TAG, "Clicked Previous");
+
+        } else if (intent.getAction().equals(ACTION_PAUSE)) {
+            LogHelper.i(TAG, "Received pause foreground intent ");
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(PLAYBACK_NOTIFICATION_ID, getNotification(false));
+
+        } else if (intent.getAction().equals("NEXT_ACTION")) {
+            LogHelper.i(TAG, "Clicked Next");
+
+        } else if (intent.getAction().equals(ACTION_STOP)) {
+            LogHelper.i(TAG, "Received stop foreground intent");
+
+            stopForeground(true);
+            stopSelf();
+        }
+
+        return START_STICKY;
+    }
+
+    private Notification getNotification(boolean isPlaying) {
+        Intent notificationIntent = new Intent(this, ReadAndListenActivity.class);
+        notificationIntent.setAction("MAIN_ACTION");
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        Intent previousIntent = new Intent(this, MusicService.class);
+        previousIntent.setAction("PREV_ACTION");
+        PendingIntent previousPendingIntent = PendingIntent.getService(this, 0, previousIntent, 0);
+
+        Intent playIntent = new Intent(this, MusicService.class);
+        playIntent.setAction(isPlaying ? ACTION_PAUSE : ACTION_PLAY);
+        PendingIntent playPendingIntent = PendingIntent.getService(this, 0, playIntent, 0);
+
+        Intent nextIntent = new Intent(this, MusicService.class);
+        nextIntent.setAction("NEXT_ACTION");
+        PendingIntent nextPendingIntent = PendingIntent.getService(this, 0, nextIntent, 0);
+
+        // Using RemoteViews to bind custom layouts into Notification
+        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.playback_notification);
+        remoteViews.setTextViewText(R.id.textViewTitle, isPlaying ? "playing..." : "paused");
+
+        // TODO design a more sophisticated notification layout with close (X) on top-right corner and media control buttons
+        // see http://stackoverflow.com/a/18558404 for details
+
+        return new NotificationCompat.Builder(this)
+                .setContentTitle("title")
+                .setTicker("ticker")
+                .setContent(remoteViews)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher), 128, 128, false))
+                .setContentIntent(pendingIntent)
+                .setOngoing(true)
+//                .addAction(android.R.drawable.ic_media_previous, "", previousPendingIntent)
+//                .addAction(R.id.play_pause, "", playPendingIntent)
+//                .addAction(android.R.drawable.ic_media_next, "", nextPendingIntent)
+                .build();
+    }
+
+    public String getDataSource() {
+        return dataSource;
+    }
+
+    private void handlePlayRequest(String dataSource) {
+        mediaPlayer = new MediaPlayer();
+
+        mediaPlayer.setWakeMode(getApplicationContext(),
+                PowerManager.PARTIAL_WAKE_LOCK);
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        mediaPlayer.setOnPreparedListener(this);
+        mediaPlayer.setOnCompletionListener(this);
+        mediaPlayer.setOnErrorListener(this);
+
+        try {
+            this.dataSource = dataSource;
+            mediaPlayer.setDataSource(dataSource);
+            mediaPlayer.prepareAsync(); // prepare async to not block main thread
+
+        } catch (java.io.IOException e) {
+            LogHelper.e(TAG, e.getMessage());
+        }
+    }
+
+    public void stopPlayback() {
+        if (mediaPlayer != null) {
+            stopForeground(true);
+
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
 
     /**
      * Class for clients to access.  Because we know this service always
@@ -92,52 +206,18 @@ public class MusicService extends Service implements
         }
     }
 
-    @Override
-    public void onCompletion(MediaPlayer mp) {
+    public interface OnMediaStateChangedListener {
+        void onMediaStateChanged(int state);
     }
 
-    @Override
-    public boolean onError(MediaPlayer mp, int what, int extra) {
-        return false;
+    protected OnMediaStateChangedListener onMediaStateChangedListener = null;
+
+    public void setOnMediaStateChangedListener(OnMediaStateChangedListener listener) {
+        onMediaStateChangedListener = listener;
     }
 
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        if (intent.getAction().equals(ACTION_PLAY)) {
-            mediaPlayer = new MediaPlayer();
-
-            mediaPlayer.setWakeMode(getApplicationContext(),
-                    PowerManager.PARTIAL_WAKE_LOCK);
-            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-
-            mediaPlayer.setOnPreparedListener(this);
-            mediaPlayer.setOnCompletionListener(this);
-            mediaPlayer.setOnErrorListener(this);
-
-            try {
-                dataSource = intent.getStringExtra("dataSource");
-                mediaPlayer.setDataSource(dataSource);
-                mediaPlayer.prepareAsync(); // prepare async to not block main thread
-
-            } catch (java.io.IOException e) {
-                LogHelper.e(TAG, e.getMessage());
-            }
-        }
-
-        // We want this service to continue running until it is explicitly stopped, so return sticky.
-        return START_STICKY;
-    }
-
-    public String getDataSource() {
-        return dataSource;
-    }
-
-    public void stopPlayback() {
-        if (mediaPlayer != null) {
-            stopForeground(true);
-
-            mediaPlayer.stop();
-            mediaPlayer.release();
-            mediaPlayer = null;
-        }
+    public void removeOnMediaStateChangedListener(OnMediaStateChangedListener listener) {
+        if(listener == onMediaStateChangedListener)
+            onMediaStateChangedListener = null;
     }
 }
--- app/src/main/java/me/ali/coolenglishmagazine/ReadAndListenActivity.java
+++ app/src/main/java/me/ali/coolenglishmagazine/ReadAndListenActivity.java
@@ -4,16 +4,21 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.media.session.PlaybackState;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import me.ali.coolenglishmagazine.util.LogHelper;
 
 
-public class ReadAndListenActivity extends AppCompatActivity {
+public class ReadAndListenActivity extends AppCompatActivity implements View.OnClickListener, MusicService.OnMediaStateChangedListener {
 
     private static final String TAG = LogHelper.makeLogTag(ReadAndListenActivity.class);
 
@@ -30,6 +35,9 @@ public class ReadAndListenActivity extends AppCompatActivity {
         WebView webView = (WebView) findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("file:///mnt/sdcard/cool-english-magazine/swimming-squirrel.html");
+
+        ImageView startButton = (ImageView) findViewById(R.id.play_pause);
+        startButton.setOnClickListener(this);
     }
 
     @Override
@@ -40,11 +48,16 @@ public class ReadAndListenActivity extends AppCompatActivity {
         bindToMusicService();
     }
 
+    /**
+     * this callback is guaranteed to get called before any kind of process killing. see
+     * http://developer.android.com/intl/pt-br/reference/android/app/Activity.html#ActivityLifecycle
+     */
     @Override
     protected void onStop() {
         super.onStop();
         LogHelper.d(TAG, "onStop");
 
+        musicService.removeOnMediaStateChangedListener(ReadAndListenActivity.this);
         unbindMusicService();
     }
 
@@ -54,16 +67,16 @@ public class ReadAndListenActivity extends AppCompatActivity {
     private ServiceConnection musicServiceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
-            MusicService.MusicBinder binder = (MusicService.MusicBinder) service;
-            musicService = binder.getService();
-
+            musicService = ((MusicService.MusicBinder) service).getService();
             LogHelper.d(TAG, "Bound to music service.");
 
-            final String dataSource = "file:///mnt/sdcard/cool-english-magazine/swimming-squirrel.mp3";
-            if (!dataSource.equals(musicService.getDataSource())) {
-                musicService.stopPlayback();
-            }
+            musicService.setOnMediaStateChangedListener(ReadAndListenActivity.this);
             boundToMusicService = true;
+
+//            final String dataSource = "file:///mnt/sdcard/cool-english-magazine/swimming-squirrel.mp3";
+//            if (!dataSource.equals(musicService.getDataSource())) {
+//                musicService.stopPlayback();
+//            }
         }
 
         @Override
@@ -84,6 +97,30 @@ public class ReadAndListenActivity extends AppCompatActivity {
             // Detach our existing connection.
             unbindService(musicServiceConnection);
             boundToMusicService = false;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.play_pause:
+                Intent startIntent = new Intent(ReadAndListenActivity.this, MusicService.class);
+                startIntent.setAction(MusicService.ACTION_PLAY);
+                startIntent.putExtra("dataSource", "file:///mnt/sdcard/cool-english-magazine/swimming-squirrel.mp3");
+                startService(startIntent);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onMediaStateChanged(int state) {
+        LogHelper.i(TAG, "media playback state: ", state);
+
+        if(state == PlaybackState.STATE_PLAYING) {
+
         }
     }
 }
