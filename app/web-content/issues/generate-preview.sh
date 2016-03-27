4458ed022f7d2c2ea429bfcca43ee33e97e49116
Ali Tavakol <ali.tavakol@gmail.com>
2016-03-27 15:06:38 +0430

    rename preview generator jar to be meaningful
    add usage comments to generate-preview.sh

--- app/web-content/issues/generate-preview.sh
+++ app/web-content/issues/generate-preview.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# usage:
+# generate-preview.sh <issues root path> <first issue number to include in zip> <last issue number to include in zip>
+
 pushd $1
-java -jar cem-zip-issues-folder.jar --min-issue-number $2 --max-issue-number $3 -o issues-preview-$2-$3.zip
+java -jar generate-issues-preview.jar --min-issue-number $2 --max-issue-number $3 -o issues-preview-$2-$3.zip
 popd
