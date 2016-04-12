ea616e48f06d8c0dd2339da9623b01fd412d4e12
Ali Tavakol <ali.tavakol@gmail.com>
2016-04-12 16:32:16 +0430

    make preview archive by Rails server
    dedicated java JAR had problems. in addition, it becomes more simple to get rid of it.

--- app/web-content/issues/generate-preview.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# usage:
-# generate-preview.sh <issues root path> <first issue number to include in zip> <last issue number to include in zip>
-
-pushd $1
-java -jar generate-issues-preview.jar --min-issue-number $2 --max-issue-number $3 -o issues-preview-$2-$3.zip
-popd
