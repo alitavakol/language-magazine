7e58e5909c4c4847b3b9401a3251dc11617d0ec4
Ali Tavakol <ali.tavakol@gmail.com>
2016-08-05 19:19:55 +0430

    splash screen

---
+++ app/src/main/java/me/ali/coolenglishmagazine/SplashActivity.java
@@ -0,0 +1,40 @@
+package me.ali.coolenglishmagazine;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        int delay = 800;
+
+        // show splash screen more time on first start
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if (!preferences.getBoolean("splash_shown", false)) {
+            delay = 2000;
+            preferences.edit().putBoolean("splash_shown", true).apply();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, RootActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, delay);
+    }
+}
